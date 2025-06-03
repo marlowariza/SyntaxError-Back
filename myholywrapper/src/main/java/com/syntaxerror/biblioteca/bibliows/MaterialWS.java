ce50a18033b9b2495c6193dfab37a6fbb47aa8aa
PapayaModerna <jalvaradoaranguri@gmail.com>
2025-06-03 18:55:42 -0500

    Cambios MaterialWS

--- myholywrapper/src/main/java/com/syntaxerror/biblioteca/bibliows/MaterialWS.java
+++ myholywrapper/src/main/java/com/syntaxerror/biblioteca/bibliows/MaterialWS.java
@@ -2,6 +2,7 @@ package com.syntaxerror.biblioteca.bibliows;
 
 import com.syntaxerror.biblioteca.business.MaterialBO;
 import com.syntaxerror.biblioteca.business.MaterialCreadorBO;
+import com.syntaxerror.biblioteca.business.MaterialTemaBO;
 import com.syntaxerror.biblioteca.business.util.BusinessException;
 import com.syntaxerror.biblioteca.model.CreadorDTO;
 import com.syntaxerror.biblioteca.model.EjemplarDTO;
@@ -25,11 +26,14 @@ public class MaterialWS {
 
     private final MaterialBO materialBO;
     private final MaterialCreadorBO materialCreadorBO;
+    private final MaterialTemaBO materialTemaBO;
 
     public MaterialWS() {
         materialBO = new MaterialBO();
         materialCreadorBO = new MaterialCreadorBO();
+        materialTemaBO =new MaterialTemaBO();
     }
+    
     @WebMethod(operationName = "insertarMaterial")
     public int insertarMaterial(
         @WebParam(name = "titulo") String titulo,
@@ -133,6 +137,38 @@ public class MaterialWS {
             return materialBO.listarPorCaracter_Creador(car);
         } catch (BusinessException e) {
             throw new WebServiceException("Error al listar Material por car_creadores" + e.getMessage());
+        }
+    }
+    @WebMethod(operationName = "asociarMaterialTema")
+    public Integer asociarMaterialPorTema(
+        @WebParam(name = "idMaterial") Integer idMaterial,
+        @WebParam(name = "idTema") Integer idTema
+    ) throws BusinessException {
+        try {
+            if (materialTemaBO.existeRelacion(idMaterial, idTema)) {
+                throw new BusinessException("La relación entre el material y el tema ya existe.");
+            }
+            return materialTemaBO.asociar(idMaterial, idTema);
+        } catch (BusinessException e) {
+            throw new WebServiceException("Error al asociar material con tema: " + e.getMessage());
+        } catch (Exception e) {
+            throw new WebServiceException("Error inesperado al asociar material con tema: " + e.getMessage());
+        }
+    }
+    @WebMethod(operationName = "asociarMaterialCreador")
+    public Integer asociarMaterialPorCreador(
+            @WebParam(name = "idMaterial") Integer idMaterial,
+            @WebParam(name = "idCreador") Integer idCreador
+    ) throws BusinessException {
+        try {
+            if (materialCreadorBO.existeRelacion(idMaterial, idCreador)) {
+                throw new BusinessException("La relación entre el material y el creador ya existe.");
+            }
+            return materialCreadorBO.asociar(idMaterial, idCreador);
+        } catch (BusinessException e) {
+            throw new WebServiceException("Error al asociar material con creador: " + e.getMessage());
+        } catch (Exception e) {
+            throw new WebServiceException("Error inesperado al asociar material con creador: " + e.getMessage());
         }
     }
 }
