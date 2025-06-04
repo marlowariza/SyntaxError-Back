406d0e707e09d3d7125d3c1f91548a22c49cdfaf
marlowariza <arizamarlow@gmail.com>
2025-06-03 22:00:52 -0500

    Update MaterialCreadorDAOImpl.java

--- myholyback/src/main/java/com/syntaxerror/biblioteca/persistance/dao/impl/MaterialCreadorDAOImpl.java
+++ myholyback/src/main/java/com/syntaxerror/biblioteca/persistance/dao/impl/MaterialCreadorDAOImpl.java
@@ -15,7 +15,7 @@ public class MaterialCreadorDAOImpl extends RelacionDAOImplBase<MaterialDTO, Cre
     private static final Logger LOGGER = Logger.getLogger(MaterialCreadorDAOImpl.class.getName());
     
     public MaterialCreadorDAOImpl() {
-        super("BIB_MATERIAL_CREADOR", "MATERIAL_IDMATERIAL", "CREADOR_IDCREADOR", "BIB_MATERIAL", "BIB_AUTOR");
+        super("BIB_MATERIAL_CREADOR", "MATERIAL_IDMATERIAL", "CREADOR_IDCREADOR", "BIB_MATERIAL", "BIB_CREADOR");
     }
 
     @Override
